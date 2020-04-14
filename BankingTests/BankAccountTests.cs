2af792752befefa515c8a15ca2269a4b423fd75f
Kurt Risley <kurtdrisley@gmail.com>
2020-04-14 13:42:58 -0600

    Initial Commit

---
+++ BankingTests/BankAccountTests.cs
@@ -0,0 +1,49 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void NewAccountsHaveAppropriateBalance()
+        {
+            // Write the Code Tou Wish Tou Had (WTCYWYH) (Corey Haines)
+
+            BankAccount account = new BankAccount();
+
+            decimal balance = account.GetBalance();
+
+            Assert.Equal(1200M, balance);
+        }
+
+        [Fact]
+        public void DepositingIncreasesBalance()
+        {
+            // (Arrange) Given - I have a new account and I have the balance of that account
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+            // (Act) When I deposit $100.
+            account.Deposit(amountToDeposit);
+            // (Assert) Then the accounts balance should be the opening balance plus 100.
+            Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+        }
+
+        [Fact]
+        public void WithdrawalsDecreaseBalsnce()
+        {
+            // (Arrange) Given - I have a new account and I have the balance of that account
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToWithdraw = 100M;
+            // (Act) When I deposit $100.
+            account.Withdraw(amountToWithdraw);
+            // (Assert) Then the accounts balance should be the opening balance plus 100.
+            Assert.Equal(openingBalance + amountToWithdraw, account.GetBalance());
+        }
+    }
+}
